ab0d8f86ce91c6d4be241a8355179b51abc575ed
willythecat <willythecat@protonmail.com>
2018-10-15 07:16:54 +0200

    Bug fix

--- src/validation.cpp
+++ src/validation.cpp
@@ -3245,7 +3245,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 
     // Check transactions
     for (const auto& tx : block.vtx)
-        if (!CheckTransaction(*tx, state, false, blockhash))
+        if (!CheckTransaction(*tx, state, true, blockhash))
             return state.Invalid(false, state.GetRejectCode(), state.GetRejectReason(),
                                  strprintf("Transaction check failed (tx hash %s) %s", tx->GetHash().ToString(), state.GetDebugMessage()));
 
