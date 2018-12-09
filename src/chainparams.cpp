a2ac952dfd961d1fd0deeeb21c4c837bd278009e
willythecat <willythecat@protonmail.com>
2018-12-09 13:28:47 +0100

    Added second DNS seeder

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -225,7 +225,8 @@ copy merkle root
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("dnsseed.choosebitcash.com"); 
+        vSeeds.emplace_back("dnsseed.choosebitcash.com");
+        vSeeds.emplace_back("dnsseed2.choosebitcash.com");  
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
